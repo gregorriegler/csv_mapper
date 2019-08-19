135ed7b9b72af63c406e161347879d58eb6ae0a3
Johannes Hartl <johannes.hartl@ebcont.com>
2019-08-19 11:17:50 +0200

    adding tests for modes

--- csv_mapper/app.py
+++ csv_mapper/app.py
@@ -24,27 +24,27 @@ def write_csv(content):
 
 def apply_rule(content, rule):
     column = int(rule[0])
-    mode = rule[3]
+    mode = rule[3].lower()
+    wordToReplace = rule[1].lower()
 
     for row_number, row in enumerate(content):
         if row_number == 0: continue
-        # split column into a word array (takes whitespace as the default delimiter)
-        word_array = row[column].split();
+        #split column into a lower case word array(whitespace as delimiter)
+        word_array = row[column].lower().split(' ');
 
         if mode == 'replace_column':
             for word in word_array:
-                if word == rule[1]:
+                if word == wordToReplace:
                     content[row_number][column] = rule[2]
                     break
 
         elif mode == 'replace_word':
+            for word_number, word in enumerate(word_array):
+                if word == wordToReplace:
+                    word_array[word_number] = rule[2]
+
             seperator = " "
-            for word in word_array:
-                if word == rule[1]:
-                    word = rule[1]
             content[row_number][column] = seperator.join(word_array)
-
-
 
 
 def map_content(content, rules):
--- test/csv_mapper_test.py
+++ test/csv_mapper_test.py
@@ -18,35 +18,42 @@ class TestCsvMapper:
 
     def test_skip_first_line(self):
         csv_content = [["hello", "world"], ["hello", "world"]]
-        rules = [[0, "hello", "helloooo", "replace_column"]]
+        rules = [[0, "hello", "helloooo", "replace_column"], [1, "world", "max", "replace_word"]]
 
         result = map_content(csv_content, rules)
-        assert result == [["hello", "world"], ["helloooo", "world"]]
+        assert result == [["hello", "world"], ["helloooo", "max"]]
 
     def test_not_replace_content(self):
-        csv_content = [["h1", "h2"], ["hello", "world"]]
-        rules = [[0, "Hello", "helloooo", "replace_column"]]
+        csv_content = [["h1", "h2"], ["hello", "hello world"]]
+        rules = [[0, "henllo", "helloooo", "replace_column"], [1, "w0rld", "hello", "replace_word"]]
 
         result = map_content(csv_content, rules)
-        assert result == [["h1", "h2"], ["hello", "world"]]
+        assert result == [["h1", "h2"], ["hello", "hello world"]]
 
     def test_replace_multiple_content(self):
-        csv_content = [["h1", "h2"], ["hello", "world"], ["hell0", "world"], ["hello", "w0rld"]]
-        rules = [[0, "hello", "helloooo", "replace_column"]]
+        csv_content = [["h1", "h2"], ["hello world", "worldd"], ["hell0", "hello world"], ["hello", "w0rld"]]
+        rules = [[0, "hello", "helloooo", "replace_column"], [1, "world", "max", "replace_word"]]
 
         result = map_content(csv_content, rules)
-        assert result == [["h1", "h2"], ["helloooo", "world"], ["hell0", "world"], ["helloooo", "w0rld"]]
+        assert result == [["h1", "h2"], ["helloooo", "worldd"], ["hell0", "hello max"], ["helloooo", "w0rld"]]
+
+    def test_replace_content_regardless_of_case(self):
+        csv_content = [["h1", "h2"], ["heLlO world", "worldd"], ["hell0", "hello WoRlD"], ["hello", "w0rld"]]
+        rules = [[0, "HelLo", "helloooo", "replace_column"], [1, "wOrld", "MAX", "replace_word"]]
+
+        result = map_content(csv_content, rules)
+        assert result == [["h1", "h2"], ["helloooo", "worldd"], ["hell0", "hello MAX"], ["helloooo", "w0rld"]]
 
     def test_not_replace_content_in_different_columns(self):
-        csv_content = [["h1", "h2", "h3"], ["hello", "hello", "henlo"]]
-        rules = [[0, "hello", "helloooo", "replace_column"]]
+        csv_content = [["h1", "h2", "h3"], ["hello", "hello", "hello"]]
+        rules = [[0, "hello", "helloooo", "replace_column"], [1, "hello", "max", "replace_word"]]
 
         result = map_content(csv_content, rules)
-        assert result == [["h1", "h2", "h3"], ["helloooo", "hello", "henlo"]]
-        
+        assert result == [["h1", "h2", "h3"], ["helloooo", "max", "hello"]]
+
     def test_replace_content_in_multiple_columns(self):
         csv_content = [["h1", "h2"], ["hello", "world"]]
-        rules = [[0, "hello", "helloooo", "replace_column"], [1, "world", "wooorld", "replace_column"]]
+        rules = [[0, "hello", "helloooo", "replace_column"], [1, "world", "wooorld", "replace_word"]]
 
         result = map_content(csv_content, rules)
         assert result == [["h1", "h2"], ["helloooo", "wooorld"]]
